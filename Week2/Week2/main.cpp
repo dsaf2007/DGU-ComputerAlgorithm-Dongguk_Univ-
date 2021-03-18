6c3e98ae9b93698a9b05ac23d78b57b98ec92754
dsaf2007 <dsaf2007@naver.com>
2021-03-18 11:05:21 +0900

    11:05

---
+++ Week2/Week2/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <time.h>
+//2016112154 Á¤µ¿±¸
+
+void swap(int a_, int b_);
+void BubbleSort(int arr[]);
+void BubbleSortRecur(int arr[]);
+
+
+int main()
+{
+	std::cout << "Hello world";
+
+}
+
+
+
+
+void swap(int a_, int b_)
+{
+	int temp = a_;
+	a_ = b_;
+	b_ = temp;
+}
+
+void BubbleSort(int arr[])
+{
+	for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+		for(int j = 0 ; j < i ; j++)
+		{
+			if (arr[j] > arr[j + 1])swap(arr[j], arr[j + 1]);
+		}
+}
+
+void BubbleSortRecur(int arr[])
+{
+
+}
