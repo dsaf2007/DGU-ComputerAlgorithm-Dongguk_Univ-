34051e3c5838aa754cac3dae753f2f77e4c08ca4
dsaf2007 <dsaf2007@naver.com>
2021-03-31 16:44:05 +0900

    heap.h

---
+++ Week3/heap.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#define defMaxNum 11
+class Heap
+{
+public:
+	Heap(int maxNumber = defMaxNum)
+	{
+		element = new int[maxNumber];
+		size = 0;
+		maxSize = maxNumber;
+	}
+
+	~Heap()
+	{
+		delete[] element;
+	}
+
+	void insert(const int newElement)
+	{
+		int i;
+		i = ++size;
+		while ((i != 1) && (newElement > element[i / 2]))
+		{
+			element[i] = element[i / 2];
+			i /= 2;
+		}
+		element[i] = newElement;
+	}
+
+	int removeMax()
+	{
+		if (!empty())
+		{
+			int parent, child;
+			int temp = element[size];
+			int result = element[1];
+			parent = 1;
+			child = 2;
+
+			while (child <= size)
+			{
+				if ((child < size) && (element[child] < element[child + 1]))
+					child++;
+				if (temp >= element[child])
+					break;
+				element[parent] = element[child];
+				parent = child;
+				child *= 2;
+			}
+			element[parent] = temp;
+			size--;
+			return result;
+		}
+		else
+			return 0;
+	}
+
+	// Heap status operations
+	int empty() const
+	{
+		return (size == 0);
+	}
+
+	int maxSize, size;
+	int* element;
+};
--- Week3/main.cpp
+++ Week3/main.cpp
@@ -1,77 +1,17 @@
 #include <iostream>
+#include "heap.h"
 
-#define defMaxNum 11
+void exec_heap();
 
-class Heap
+int main()
 {
-public:
-	Heap(int maxNumber = defMaxNum)
-	{
-		element = new int[maxNumber];
-		size = 0;
-		maxSize = maxNumber;
-	}
-
-	~Heap()
-	{
-		delete[] element;
-	}
-
-	void insert(const int newElement)
-	{
-		int i;
-		i = ++size;
-		while ((i != 1) && (newElement > element[i / 2]))
-		{
-			element[i] = element[i / 2];
-			i /= 2;
-		}
-		element[i] = newElement;
-	}
-
-	int removeMax()
-	{
-		if (!empty())
-		{
-			int parent, child;
-			int temp = element[size];
-			int result = element[1];
-			parent = 1;
-			child = 2;
-
-			while (child <= size)
-			{
-				if ((child < size) && (element[child] < element[child + 1]))
-					child++;
-				if (temp >= element[child])
-					break;
-				element[parent] = element[child];
-				parent = child;
-				child *= 2;
-			}
-			element[parent] = temp;
-			size--;
-			return result;
-		}
-		else
-			return 0;
-	}
-
-	// Heap status operations
-	int empty() const
-	{
-		return (size == 0);
-	}
-
-	int maxSize, size;
-	int* element;
-};
+	exec_heap();
+}
 
 void exec_heap()
 {
 	int n;
 	Heap heap;
-	//std::cin >> N;
 	int arr[10];
 
 	for (int i = 0; i < 10; i++)
@@ -85,11 +25,6 @@ void exec_heap()
 	}
 	for (int i = 0; i < 10; i++)
 	{
-		std::cout << arr[i]<<" ";
+		std::cout << arr[i] << " ";
 	}std::cout << "\n";
-}
-
-int main()
-{
-	exec_heap();
 }
